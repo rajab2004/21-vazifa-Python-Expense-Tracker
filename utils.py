e88f758b3a8a0a76f8371479b08eb4aa319d9bf1
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-06-30 08:23:39 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,21 @@
+import os
 
+FILENAME = "expenses.csv"
+
+def add_expense(date, category, amount):
+    pass
+
+def read_all_expenses():
+    pass
+
+def calculate_total(expenses):
+    pass
+
+def filter_by_date(expenses, search_date):
+    pass
+
+def filter_by_category(expenses, search_category):
+    pass
+
+def format_expense(row):
+    pass
